bdaf8292f7bc7826aef9742209bec8df22a72305
Mayur <mayurharisinghani@gmail.com>
2025-02-03 14:03:00 -0700

    Completed Part 1

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,58 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int intOperandA = 3;
+        int intOperandB = 6;
+        int intSum = 7;
+        int intProduct = 12;
+        int intDifference = 15;
+        int intQuotient = 17;
+        int intModulo = 19;
+        double doubleOperandA = 3.50; //This is a declaration and initial assignment
+        double doubleOperandB = 4.20; //This is a declaration and initial assignment
+        double doubleSum = 7.12; //This is a declaration and initial assignment
+        double doubleProduct = 8.45; //This is a declaration and initial assignment
+        double doubleDifference = 9.16; //This is a declaration and initial assignment
+        double doubleQuotient = 13.45; //This is a declaration and initial assignment
+        double doubleModulo = 16.57; //This is a declaration and initial assignment
+        intSum = intOperandA + intOperandB; //Assignment that uses the arithmetic operator +
+        System.out.println("The sum using ints of " + intOperandA + " and " + intOperandB + " is " +intSum); //Output statement
+        intProduct = intOperandA * intOperandB; //Assignment that uses the arithmetic operator *
+        System.out.println("The product using ints of " + intOperandA + " and " + intOperandB + " is " +intProduct); //Output statement
+        intDifference = intOperandA - intOperandB; //Assignment that uses the arithmetic operator -
+        System.out.println("The difference using ints of " + intOperandA + " and " + intOperandB + " is " +intDifference); //Output statement
+        intQuotient = intOperandA / intOperandB; //Assignment that uses the arithmetic operator /
+        System.out.println("The quotient using ints of " + intOperandA + " and " + intOperandB + " is " +intQuotient); //Output statement
+        intModulo = intOperandA % intOperandB; //Assignment that uses the arithmetic operator %
+        System.out.println("The modulo using ints of " + intOperandA + " and " + intOperandB + " is " +intModulo); //Output statement
+        doubleSum = doubleOperandA + doubleOperandB; //Assignment that uses the arithmetic operator +
+        System.out.println("The sum using double variables of " + doubleOperandA + " and " + doubleOperandB + " is " +doubleSum); //Output statement
+        doubleProduct = doubleOperandA * doubleOperandB; //Assignment that uses the arithmetic operator *
+        System.out.println("The product using double variables of " + doubleOperandA + " and " + doubleOperandB + " is " +doubleProduct); //Output statement
+        doubleDifference = doubleOperandA - doubleOperandB; //Assignment that uses the arithmetic operator -
+        System.out.println("The difference using double variables of " + doubleOperandA + " and " + doubleOperandB + " is " +doubleDifference); //Output statement
+        doubleQuotient = doubleOperandA / doubleOperandB; //Assignment that uses the arithmetic operator /
+        System.out.println("The quotient using double variables of " + doubleOperandA + " and " + doubleOperandB + " is " +doubleQuotient); //Output statement
+        doubleModulo = doubleOperandA % doubleOperandB; //Assignment that uses the arithmetic operator %
+        System.out.println("The modulo using double variables of " + doubleOperandA + " and " + doubleOperandB + " is " +doubleModulo); //Output statement
+        double myLunchCost = 12.50;
+        System.out.println("The cost of my lunch is: " +myLunchCost);
+        int numberOfKids = 2;
+        System.out.println("The number of kids in your family is: " +numberOfKids);
+        boolean isRaining = false;
+        System.out.println("Is it raining? " +isRaining);
+        double priceOfGallon = 5.80;
+        System.out.println("The price of a gallon of gas is $" +priceOfGallon);
+        double favouriteNumber = 3.14;
+        System.out.println("Your favourite number is: " +favouriteNumber);
+        double shoeSize = 10.5;
+        System.out.println("Your shoe size is: " +shoeSize);
+        int birthMonth = 6;
+        System.out.println("Your birth month is: " +birthMonth);
+        String firstName = "Mayur";
+        String lastName = "Harisinghani";
+        String fullName = firstName + " " +lastName;
+        System.out.println("Your full name is: " +fullName);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
